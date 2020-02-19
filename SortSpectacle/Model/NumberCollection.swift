17325792b06a07facd0a02cde2ce0bec45a45ea1
Antti Juustila <antti.juustila@gmail.com>
2020-02-19 17:37:11 +0200

    Basics and unit tests for NumberCollection implemented.

--- SortSpectacle/Model/NumberCollection.swift
+++ SortSpectacle/Model/NumberCollection.swift
@@ -20,17 +20,23 @@ class NumberCollection {
    
    func generateNumbers(count : Int) -> Void {
       numbers = []
-      for _ in 0...count {
+      numbers.reserveCapacity(count)
+      for _ in 0..<count {
          numbers.append(Int.random(in: numberRange))
       }
    }
    
-   func numberCount() -> Int {
+   func clear() -> Void {
+      numbers = []
+   }
+   
+   func count() -> Int {
       numbers.count
    }
    
    func number(index : Int) -> Int {
-      numbers[index]
+      precondition(index >= 0 && index < count())
+      return numbers[index]
    }
    
    func max() -> Int {
@@ -43,4 +49,14 @@ class NumberCollection {
       return largest
    }
    
+   func swap(from index1 : Int, to index2 : Int) -> Void {
+      precondition(index1 >= 0 && index1 < count() && index2 >= 0 && index2 < count())
+      let temp = numbers[index1]
+      numbers[index1] = numbers[index2]
+      numbers[index2] = temp
+   }
+   
+   func shuffle() -> Void {
+      numbers.shuffle()
+   }
 }
--- SortSpectacleTests/SortSpectacleTests.swift
+++ SortSpectacleTests/SortSpectacleTests.swift
@@ -9,26 +9,55 @@
 import XCTest
 @testable import SortSpectacle
 
-class SortSpectacleTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+class SortSpectacleCollectionTests: XCTestCase {
+   
+   private var numbers : NumberCollection!
+   private var range = 0
+   private var count = 0
+   
+   
+   override func setUp() {
+      super.setUp()
+      range = Int.random(in: 2...1000)
+      count = Int.random(in: 2...5000)
+      numbers = NumberCollection(range: range)
+      numbers.generateNumbers(count: count)
+   }
+   
+   override func tearDown() {
+      numbers = nil
+   }
+   
+   func test1EmptyCollection() {
+      numbers.clear()
+      XCTAssertEqual(numbers.count(), 0)
+   }
+   
+   func testNumbersSetup() {
+      XCTAssertEqual(numbers.count(), count)
+      let maxValue = numbers.max()
+      XCTAssertLessThan(maxValue, range+1)
+   }
+   
+   func testSwappingValues() {
+      let randomIndex1 = Int.random(in: 0...numbers.count())
+      var randomIndex2 = 0
+      repeat {
+         randomIndex2 = Int.random(in: 0...numbers.count())
+      } while randomIndex1 == randomIndex2
+      
+      let value1 = numbers.number(index: randomIndex1)
+      let value2 = numbers.number(index: randomIndex2)
+      numbers.swap(from: randomIndex1, to: randomIndex2)
+      XCTAssertEqual(numbers.number(index: randomIndex2), value1)
+      XCTAssertEqual(numbers.number(index: randomIndex1), value2)
+   }
+   
+   func testPerformanceExample() {
+      // This is an example of a performance test case.
+      self.measure {
+         _ = numbers.max()
+      }
+   }
+   
 }
