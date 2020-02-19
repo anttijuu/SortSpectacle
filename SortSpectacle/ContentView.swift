41080b52b9b9092bdcb03866e995c17256182661
Antti Juustila <antti.juustila@gmail.com>
2020-02-19 18:30:49 +0200

    Added skeleton code for drawing the UI.

--- SortSpectacle/ContentView.swift
+++ SortSpectacle/ContentView.swift
@@ -8,10 +8,23 @@
 
 import SwiftUI
 
+struct BackgroundShape : Shape {
+   func path(in rect: CGRect) -> Path {
+      var path = Path()
+      path.addRect(rect)
+      path.move(to: CGPoint(x: rect.origin.x+rect.width/2, y: rect.origin.y))
+      path.addLine(to: CGPoint(x: rect.origin.x+rect.width/2, y: rect.origin.y+rect.height))
+      return path
+   }
+}
 
 struct ContentView: View {
+   
+   private var numbers = NumberCollection(range: 100)
+   
     var body: some View {
-        Text("Hello, World!")
+      BackgroundShape()
+         .stroke()
     }
 }
 
