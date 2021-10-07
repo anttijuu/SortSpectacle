1e252ffd4d78c2ec10298e48d54b95546828d094
Antti Juustila <antti.juustila@gmail.com>
2021-10-07 22:19:27 +0300

    Added docs for Heapsort

--- SortSpectacle/Model/HeapSort.swift
+++ SortSpectacle/Model/HeapSort.swift
@@ -8,17 +8,33 @@
 
 import Foundation
 
+/**
+ Heapsort is an improved selection sort: like selection sort, heapsort divides its input into a sorted and an unsorted region, and it iteratively shrinks the unsorted region by extracting the largest element from it and inserting it into the sorted region.
+
+ Unlike selection sort, heapsort does not waste time with a linear-time scan of the unsorted region; rather, heap sort maintains the unsorted region in a heap data structure (binary tree) to more quickly find the largest element in each step.
+
+ Although somewhat slower in practice on most machines than a well-implemented quicksort, it has the advantage of a more favorable worst-case O(n log n) runtime. Heapsort is an in-place algorithm, but it is not a stable sort.
+
+ See https://en.wikipedia.org/wiki/Heapsort
+ */
 struct HeapSort: SortMethod {
+   /**
+    Initializes the Heapsort with the size of the array to sort.
+    - parameter arraySize The size of the array
+    */
    init(arraySize: Int) {
       size = arraySize
    }
 
+   /// The array size.
    var size: Int
 
+   /// Name of the sorting method.
    var name: String {
       "HeapSort"
    }
 
+   /// Description of the sorting method.
    var description: String {
       """
       Heapsort is an improved selection sort: like selection sort, heapsort divides its input into a sorted and an unsorted region, and it iteratively shrinks the unsorted region by extracting the largest element from it and inserting it into the sorted region.
@@ -29,10 +45,12 @@ struct HeapSort: SortMethod {
       """
    }
 
+   /// Weblinks to read more information.
    var webLinks: [(String, String)] {
       [("Wikipedia on HeapSort", "https://en.wikipedia.org/wiki/Heapsort")]
    }
 
+   /// Restarts the sorting method by giving intial values to member variables.
    mutating func restart() {
       state = .heapifying
       startIndex = parent(size - 1)
@@ -41,6 +59,10 @@ struct HeapSort: SortMethod {
       state = .siftingDown
    }
 
+   /// Executes a step of the sorting.
+   /// - parameter array The array that is sorted.
+   /// - parameter swappedItems The structure to put the indexes to sort, sorting is done elsewhere.
+   /// - returns Returns true if the sorting is done.
    mutating func nextStep(array: [Int], swappedItems: inout SwappedItems) -> Bool {
 
       if size < 2 {
@@ -108,16 +130,25 @@ struct HeapSort: SortMethod {
    private var startIndex: Int = 0
    private var endIndex: Int = 0
    private var rootIndex: Int = 0
-   
+
+   /// States of the step-by-step algorithm.
    private enum State {
+      /// Is doing the heapifying before sifting down.
       case heapifying
+      /// Is in the main while loop after heapify.
       case mainWhileLoop
+      /// Is sifting down, either from heapify or from main while loop.
       case siftingDown
+      /// Work is done.
       case finished
    }
+   /// The state of the step-by-step algorithm.
    private var state: State = .heapifying
+   /// To which state to return after sifting down.
    private var stateBeforeSifting: State = .heapifying
 
+   /// The real algorithm without step-by-step execution.
+   /// - parameter array The array to sort.
    mutating func realAlgorithm(array: inout [Int]) {
       if array.count < 2 {
          return
@@ -133,6 +164,9 @@ struct HeapSort: SortMethod {
       }
    }
 
+   /// Heapify
+   /// - parameter array The array to heapify.
+   /// - parameter count The count of elements in the array.
    private func heapify( _ array: inout [Int], _ count: Int) {
       var start = parent(count - 1)
       while start >= 0 {
@@ -141,6 +175,10 @@ struct HeapSort: SortMethod {
       }
    }
 
+   /// Sifts the values to their correct places in the heap.
+   /// - parameter array The array to sort.
+   /// - parameter start The starting index.
+   /// - parameter end The ending index.
    private func siftDown(_ array: inout [Int], start: Int, end: Int) {
       var root = start
       while leftChild(root) <= end {
@@ -163,14 +201,23 @@ struct HeapSort: SortMethod {
       }
    }
 
+   /// Heap utility method to get the parent node of an index.
+   /// - parameter index The index the parent of which is looked for.
+   /// - returns The index of the parent node.
    private func parent(_ index: Int) -> Int {
       return Int(floor((Double(index - 1))/2.0))
    }
 
+   /// Heap utility method to get the left child node of an index.
+   /// - parameter index The index the left child of which is looked for.
+   /// - returns The index of the left child.
    private func leftChild(_ index: Int) -> Int {
       return 2 * index + 1
    }
 
+   /// Heap utility method to get the right child node of an index.
+   /// - parameter index The index the right child of which is looked for.
+   /// - returns The index of the right child.
    private func rightChild(_ index: Int) -> Int {
       return 2 * index + 2
    }
